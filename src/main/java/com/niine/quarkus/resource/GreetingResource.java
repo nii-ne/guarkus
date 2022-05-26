9695adf28bea96e21ca21924f54d428f92e6a1c4
Atit Attasiri <atit.att@ascendcorp.com>
2022-05-26 13:57:19 +0700

    cache https://quarkus.io/guides/cache

--- src/main/java/com/niine/quarkus/resource/GreetingResource.java
+++ src/main/java/com/niine/quarkus/resource/GreetingResource.java
@@ -1,6 +1,7 @@
 package com.niine.quarkus.resource;
 
 import com.niine.quarkus.service.GreetingService;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -27,6 +28,7 @@ public class GreetingResource {
 
     /**
      * curl -X GET http://localhost:8080/hello
+     *
      * @return Hello from RESTEasy Reactive
      */
 
@@ -38,13 +40,32 @@ public class GreetingResource {
 
     /**
      * curl -X GET http://localhost:8080/hello/quarkus
+     *
      * @return Hello quarkus
      */
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/{name}")
     public String hello(String name) {
-        return greetingService.greeting(name);
+
+        long executionStart = System.currentTimeMillis();
+        String response = greetingService.greeting(name);
+
+        long executionEnd = System.currentTimeMillis();
+        System.out.println("Execution time: " + (executionEnd - executionStart) + " ms");
+        return response;
+    }
+    @DELETE
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/{name}")
+    public String delete(String name) {
+        return greetingService.delete(name);
+    }
+
+    @DELETE
+    @Produces(MediaType.TEXT_PLAIN)
+    public String delete() {
+        return greetingService.deleteAll();
     }
 
 }
--- src/main/java/com/niine/quarkus/service/GreetingService.java
+++ src/main/java/com/niine/quarkus/service/GreetingService.java
@@ -1,11 +1,32 @@
 package com.niine.quarkus.service;
 
+import io.quarkus.cache.CacheInvalidate;
+import io.quarkus.cache.CacheInvalidateAll;
+import io.quarkus.cache.CacheKey;
 import javax.enterprise.context.ApplicationScoped;
+
+import io.quarkus.cache.CacheResult;
 
 @ApplicationScoped
 public class GreetingService {
 
+    @CacheResult(cacheName = "hello-cache")
     public String greeting(String name) {
+        try {
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
         return "Hello " + name;
+    }
+
+    @CacheInvalidate(cacheName = "hello-cache")
+    public String delete(@CacheKey String name) {
+        return "Delete " + name;
+    }
+
+    @CacheInvalidateAll(cacheName = "hello-cache")
+    public String deleteAll() {
+        return "Delete All";
     }
 }
